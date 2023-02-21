54339afa09d289df805c43f97c2b5eff4013f2d0
NexSqaud <tf2g3124@gmail.com>
2023-02-21 14:11:06 +0700

    Initial commit

---
+++ v86/libv86.js
@@ -0,0 +1,23 @@
+--2023-02-21 13:36:26--  https://github.com/copy/v86/releases/download/latest/libv86.js
+Resolving github.com... 140.82.121.4
+Connecting to github.com|140.82.121.4|:443... connected.
+HTTP request sent, awaiting response... 302 Found
+Location: https://objects.githubusercontent.com/github-production-release-asset-2e65be/14158237/db820f5a-d78b-4f46-9d52-ec07a5eb2947?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230221%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230221T063627Z&X-Amz-Expires=300&X-Amz-Signature=62e3445cab2f48deaeac443c68b3fceb53572189e1edd1be1ffa0616ec1fd0a5&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=14158237&response-content-disposition=attachment%3B%20filename%3Dlibv86.js&response-content-type=application%2Foctet-stream [following]
+--2023-02-21 13:36:27--  https://objects.githubusercontent.com/github-production-release-asset-2e65be/14158237/db820f5a-d78b-4f46-9d52-ec07a5eb2947?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230221%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230221T063627Z&X-Amz-Expires=300&X-Amz-Signature=62e3445cab2f48deaeac443c68b3fceb53572189e1edd1be1ffa0616ec1fd0a5&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=14158237&response-content-disposition=attachment%3B%20filename%3Dlibv86.js&response-content-type=application%2Foctet-stream
+Resolving objects.githubusercontent.com... 185.199.111.133, 185.199.108.133, 185.199.110.133, ...
+Connecting to objects.githubusercontent.com|185.199.111.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 377180 (368K) [application/octet-stream]
+Saving to: ‘libv86.js.1’
+
+     0K .......... .......... .......... .......... .......... 13% 42.0K 8s
+    50K .......... .......... .......... .......... .......... 27%  101K 5s
+   100K .......... .......... .......... .......... .......... 40% 94.6K 3s
+   150K .......... .......... .......... .......... .......... 54% 67.6K 2s
+   200K .......... .......... .......... .......... .......... 67% 56.7K 2s
+   250K .......... .......... .......... .......... .......... 81%  113K 1s
+   300K .......... .......... .......... .......... .......... 95% 49.4K 0s
+   350K .......... ........                                   100% 32.7M=5.3s
+
+2023-02-21 13:36:33 (69.7 KB/s) - ‘libv86.js.1’ saved [377180/377180]
+
